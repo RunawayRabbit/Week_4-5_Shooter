4f0882108a416f3849becaea4a3a7a4a7d5cfa26
ejt <RunawayRabbitGames@gmail.com>
2020-09-18 20:30:49 +0200

    Begun working on Laser Weapons

---
+++ Assets/_Game/Scripts/Weapons/LaserWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+internal class LaserWeapon : Weapon
+{
+    [SerializeField] GameObject Emitter;
+    [SerializeField] float fireRate = 0.8f;
+    
+    
+    private Coroutine _shootingCoroutine;
+    public override void StartShooting()
+    {
+        _shootingCoroutine = StartCoroutine(Shoot());
+    }
+    
+    public override void StopShooting()
+    {
+        StopCoroutine(_shootingCoroutine);
+    }
+    
+    private IEnumerator Shoot()
+    {
+        while (true)
+        {
+            
+            yield return new WaitForSeconds(fireRate);   
+        }
+    }
+}
