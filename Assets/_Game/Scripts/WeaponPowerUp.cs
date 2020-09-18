845a31380c075dcef104358d5e081cc5277bcc74
ejt <RunawayRabbitGames@gmail.com>
2020-09-18 16:36:43 +0200

    Re-organized file structure

--- Assets/_Game/Scripts/WeaponPowerUp.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class WeaponPowerUp : MonoBehaviour
-{
-    public GameObject weaponPrefab = default;
-    
-    private void OnCollisionEnter(Collision other) => Destroy(gameObject);
-}
