b71b1bdccaa90bea74f29ed5ee2529b20e078c33
ejt <RunawayRabbitGames@gmail.com>
2020-09-20 09:20:53 +0200

    drawing control points

--- Assets/_Game/Sandbox/Editor/EnemyPathEditor.cs
+++ Assets/_Game/Sandbox/Editor/EnemyPathEditor.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(EnemyPath))]
 public class EnemyPathEditor : Editor
 {
-    
+    private void OnSceneGUI()
+    {
+        EnemyPath path = (EnemyPath) target;
+        
+        Handles.color = Color.red;
+        foreach (var point in path.points)
+        {
+            Handles.SphereHandleCap(0, point, Quaternion.identity, 0.03f, EventType.Repaint);
+        }
+        
+        Handles.color = Color.blue;
+        foreach (var point in path.controlPoints)
+        {
+            Handles.SphereHandleCap(0, point, Quaternion.identity, 0.02f, EventType.Repaint);
+        }
+        
+        Handles.color = Color.green;
+        foreach (var point in path.anchorPoints)
+        {
+            Handles.SphereHandleCap(0, point, Quaternion.identity, 0.02f, EventType.Repaint);
+        }
+    }
 }
--- Assets/_Game/Sandbox/EnemyPath.cs
+++ Assets/_Game/Sandbox/EnemyPath.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class EnemyPath : MonoBehaviour
 {
-    [SerializeField] private Vector3[] points;
-    [SerializeField] private bool isClosed;
-    [SerializeField] private Vector3[] _controlPoints;
-    [SerializeField] private Vector3[] _anchorPoints;
+    [SerializeField] public Vector3[] points;
+    [SerializeField] public bool isClosed;
+    [HideInInspector] public Vector3[] controlPoints;
+    [HideInInspector] public Vector3[] anchorPoints;
 
     private void Awake()
     {
@@ -32,25 +32,25 @@ public class EnemyPath : MonoBehaviour
         const float twoThirds = 2.0f / 3.0f;
 
         if (isClosed)
-            _controlPoints = new Vector3[points.Length * 2];
+            controlPoints = new Vector3[points.Length * 2];
         else 
-            _controlPoints = new Vector3[(points.Length - 1) * 2];
+            controlPoints = new Vector3[(points.Length - 1) * 2];
 
         for (int i = 0,  j = 0;
             i < points.Length-1;
             i++, j += 2)
         {
             Vector3 delta = points[i + 1] - points[i];
-            _controlPoints[j + 0] = points[i] + delta * oneThird;
-            _controlPoints[j + 1] = points[i] + delta * twoThirds;
+            controlPoints[j + 0] = points[i] + delta * oneThird;
+            controlPoints[j + 1] = points[i] + delta * twoThirds;
         }
 
         if (isClosed)
         {
             Vector3 finalPoint = points[points.Length - 1];
             Vector3 delta = points[0] - finalPoint;
-            _controlPoints[_controlPoints.Length - 2] = finalPoint + delta * oneThird;
-            _controlPoints[_controlPoints.Length - 1] = finalPoint + delta * twoThirds;
+            controlPoints[controlPoints.Length - 2] = finalPoint + delta * oneThird;
+            controlPoints[controlPoints.Length - 1] = finalPoint + delta * twoThirds;
         }
     }
 
@@ -59,30 +59,30 @@ public class EnemyPath : MonoBehaviour
         const float oneHalf = 0.5f;
         
         if (isClosed)
-            _anchorPoints = new Vector3[points.Length+1];
+            anchorPoints = new Vector3[points.Length+1];
         else
-            _anchorPoints = new Vector3[points.Length];
+            anchorPoints = new Vector3[points.Length];
 
         if (isClosed)
         {
             // special case endpoints
-            _anchorPoints[0] =
-                (_controlPoints[0] + _controlPoints[_controlPoints.Length - 1]) * oneHalf;
+            anchorPoints[0] =
+                (controlPoints[0] + controlPoints[controlPoints.Length - 1]) * oneHalf;
             
             // @TODO: Hacky solution, probably can be improved..
-            _anchorPoints[_anchorPoints.Length - 1] = _anchorPoints[0];
+            anchorPoints[anchorPoints.Length - 1] = anchorPoints[0];
         }
         else
         {
-            _anchorPoints[0] = points[0];
-            _anchorPoints[_anchorPoints.Length - 1] = points[points.Length - 1];
+            anchorPoints[0] = points[0];
+            anchorPoints[anchorPoints.Length - 1] = points[points.Length - 1];
         }
 
         for (int i = 1, j = 1;
-            i < _anchorPoints.Length - 1;
+            i < anchorPoints.Length - 1;
             i++, j+=2)
         {
-            _anchorPoints[i] = (_controlPoints[j] + _controlPoints[j + 1]) * oneHalf;
+            anchorPoints[i] = (controlPoints[j] + controlPoints[j + 1]) * oneHalf;
         }
     }
 }
