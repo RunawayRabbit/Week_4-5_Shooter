8be68da45622ec94c16c27baafa5338abaaf70f9
ejt <RunawayRabbitGames@gmail.com>
2020-09-20 02:49:06 +0200

    Made laser line render a tiny bit cooler.

--- Assets/_Game/Scripts/Weapons/LaserShot.cs
+++ Assets/_Game/Scripts/Weapons/LaserShot.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class LaserShot : MonoBehaviour
 {
-    [SerializeField] public float lifetime = 1.0f;
-
+    public float lifetime = 1.0f;
+    [SerializeField] public Color startColor;
+    [SerializeField] public Color endColor;
+    
+    private float birth;
     private Coroutine _lifetimeTracker;
+    private LineRenderer _lineRenderer;
+    
 
     private void OnEnable()
     {
+        birth = Time.time;
+        if (!_lineRenderer) _lineRenderer = GetComponent<LineRenderer>();
         _lifetimeTracker = StartCoroutine(DeathClock());
     }
 
     private void OnDisable()
     {
         StopCoroutine(_lifetimeTracker);
+    }
+
+    private void Update()
+    {
+        float t = (Time.time - birth) / lifetime;
+        _lineRenderer.startColor = Color.Lerp(startColor, endColor, Mathf.Min(3.0f*t, 1.0f));
+        _lineRenderer.endColor = Color.Lerp(startColor, endColor, t);
     }
 
     public IEnumerator DeathClock()
