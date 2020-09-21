b6cc8be73a1b4e3d4015aa68bf20b3d82aca1f21
ejt <RunawayRabbitGames@gmail.com>
2020-09-21 05:20:53 +0200

    Moved LUT generation into BezierSpline instead of SplineFollower

--- Assets/_Game/Scripts/Enemy/SplineFollower.cs
+++ Assets/_Game/Scripts/Enemy/SplineFollower.cs
@@ -13,10 +13,6 @@ public class SplineFollower : MonoBehaviour, IEnemyMover
     private int currentWaypoint = 0;
     private Vector3[] waypoints;
 
-    // Increase this to improve the accuracy of distance-based interpolation. I recommend 30.
-    private const int LUTLength = 30;
-    private float[] _distanceLUT;
-    
     private bool pathEnded = false;
 
     public void Move()
@@ -72,82 +68,24 @@ public class SplineFollower : MonoBehaviour, IEnemyMover
     {
         if(!pathObject.TryGetComponent<BezierSpline>(out path))
             Debug.LogWarning($"SplineFollower on {gameObject.name} doesn't have a spline object defined! I have nothing to follow!");
-    
-        BuildDistanceLUT();
+    }
+
+    private void Start()
+    {
         GenerateWaypoints();
     }
 
     private void GenerateWaypoints()
     {
         waypoints = new Vector3[numWaypoints];
-        float distanceBetweenPoints = _distanceLUT[LUTLength - 1] / numWaypoints;
+        float distanceBetweenPoints = path.ArcLength / numWaypoints;
         for (int waypointIndex = 0;
             waypointIndex < numWaypoints;
             waypointIndex++)
         {
             float distance = (float)waypointIndex * distanceBetweenPoints;
-            float t = GetTFromDistance(distance);
+            float t = path.GetTFromDistance(distance);
             waypoints[waypointIndex] = path.GetWorldPoint(t);
-        }
-    }
-
-    private float GetTFromDistance(float distance)
-    {
-        if (distance <= 0.0f) return 0.0f;
-        if (distance >= _distanceLUT[LUTLength - 1]) return 1.0f;
-        
-        int LUTIndex = 0;
-        float prevDistance = 0.0f;
-        while (LUTIndex < LUTLength - 1)
-        {
-            float currentDistance = _distanceLUT[++LUTIndex];
-            if (distance < currentDistance)
-            {
-                float localT = (distance - prevDistance) / (currentDistance - prevDistance);
-                float from = (float)(LUTIndex - 1) / LUTLength;
-                float to = (float)LUTIndex / LUTLength;
-                return Mathf.Lerp(from, to, localT);
-            }
-            prevDistance = currentDistance;
-        }
-        return 1.0f;
-    }
-    
-    private float GetDistanceFromT(float t)
-    {
-        if (_distanceLUT.Length <= 1)
-        {
-            Debug.LogError("We just tried to sample an empty LUT.");
-            return -1.0f;
-        }
-
-        float scaledT = t * (LUTLength - 1);
-        int from = Mathf.FloorToInt(scaledT);
-        int to = Mathf.FloorToInt(scaledT + 1.0f);
-
-        if(from < 0)
-            return _distanceLUT[0];
-        if (to >= LUTLength)
-            return _distanceLUT[LUTLength];
-
-        return Mathf.Lerp(_distanceLUT[from], _distanceLUT[to], scaledT - from);
-    }
-    
-    private void BuildDistanceLUT()
-    {
-        // Got some help from Freya. Thanks again!
-        _distanceLUT = new float[LUTLength];
-
-        float lengthAccumulator = 0.0f;
-        Vector3 previousPoint = path.anchorPoints[0];
-        for (int i = 1; i < LUTLength; i++)
-        {
-            float t = ((float) i) / (float)(LUTLength - 1);
-            Vector3 point = path.GetPoint(t);
-            float distance = (point - previousPoint).magnitude;
-            lengthAccumulator += distance;
-            _distanceLUT[i] = lengthAccumulator;
-            previousPoint = point;
         }
     }
 }
--- Assets/_Game/Scripts/Utils/BezierSpline.cs
+++ Assets/_Game/Scripts/Utils/BezierSpline.cs
@@ -8,9 +8,14 @@ public class BezierSpline : MonoBehaviour
     [HideInInspector] public Vector3[] controlPoints;
     [HideInInspector] public Vector3[] anchorPoints;
 
+    // Increase this to improve the accuracy of distance-based interpolation. I recommend 30.
+    private const int LUTLength = 30;
+    private float[] _distanceLUT;
+    public float ArcLength => _distanceLUT[LUTLength - 1];
     private void Awake()
     {
         CalculatePoints();
+        BuildDistanceLUT();
     }
     
 #if UNITY_EDITOR
@@ -115,6 +120,67 @@ public class BezierSpline : MonoBehaviour
             i++, j+=2)
         {
             anchorPoints[i] = (controlPoints[j] + controlPoints[j + 1]) * oneHalf;
+        }
+    }
+
+
+    public float GetTFromDistance(float distance)
+    {
+        if (distance <= 0.0f) return 0.0f;
+        if (distance >= _distanceLUT[LUTLength - 1]) return 1.0f;
+        
+        int LUTIndex = 0;
+        float prevDistance = 0.0f;
+        while (LUTIndex < LUTLength - 1)
+        {
+            float currentDistance = _distanceLUT[++LUTIndex];
+            if (distance < currentDistance)
+            {
+                float localT = (distance - prevDistance) / (currentDistance - prevDistance);
+                float from = (float)(LUTIndex - 1) / LUTLength;
+                float to = (float)LUTIndex / LUTLength;
+                return Mathf.Lerp(from, to, localT);
+            }
+            prevDistance = currentDistance;
+        }
+        return 1.0f;
+    }
+    
+    private float GetDistanceFromT(float t)
+    {
+        if (_distanceLUT.Length <= 1)
+        {
+            Debug.LogError("We just tried to sample an empty LUT.");
+            return -1.0f;
+        }
+
+        float scaledT = t * (LUTLength - 1);
+        int from = Mathf.FloorToInt(scaledT);
+        int to = Mathf.FloorToInt(scaledT + 1.0f);
+
+        if(from < 0)
+            return _distanceLUT[0];
+        if (to >= LUTLength)
+            return _distanceLUT[LUTLength];
+
+        return Mathf.Lerp(_distanceLUT[from], _distanceLUT[to], scaledT - from);
+    }
+    
+    private void BuildDistanceLUT()
+    {
+        // Got some help from Freya. Thanks again!
+        _distanceLUT = new float[LUTLength];
+
+        float lengthAccumulator = 0.0f;
+        Vector3 previousPoint = anchorPoints[0];
+        for (int i = 1; i < LUTLength; i++)
+        {
+            float t = ((float) i) / (float)(LUTLength - 1);
+            Vector3 point = GetPoint(t);
+            float distance = (point - previousPoint).magnitude;
+            lengthAccumulator += distance;
+            _distanceLUT[i] = lengthAccumulator;
+            previousPoint = point;
         }
     }
 }
