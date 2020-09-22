b8c76847fbd5c54f0cf63270c908826da3a89246
ejt <RunawayRabbitGames@gmail.com>
2020-09-22 11:01:24 +0200

    Starting work on spawners

---
+++ Assets/_Game/Scripts/Enemy/WaveSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] private int enemyCount;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Vector3 offsetBetweenSpawns;
+    [SerializeField] private GameObject path;
+    [SerializeField] private bool shouldMoveWithArena = true;
+    [SerializeField] private Vector3 spawnInLocation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (var i = 0; i < enemyCount; i++) { }
+    }
+}
